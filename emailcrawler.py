4b65b9a45479fd1a2812f83ece34dab3d1da7fe1
Cunfuu <cunfuu@hotmail.com>
2022-12-02 20:51:32 +0300

    regex changes and python3 url lib config

--- emailcrawler.py
+++ emailcrawler.py
@@ -95,7 +95,7 @@ def download(max_iterations):
             ## addresses. For more information on this, have a look at
             ## the "validating-email" example.
             
-            email_expression = r"\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,6}"
+            email_expression = r"(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])"
             eregex = re.compile(email_expression)
             
             ## Find all the email addresses
